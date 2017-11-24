32981e43a047f3c0756bc38462f5076444683190
Lucasark <31750882+Lucasark@users.noreply.github.com>
2017-11-24 20:06:24 -0200

    Grafo!

--- grafo.c
+++ grafo.c
@@ -1,16 +1,21 @@
 #include "grafo.h"
-#include "fila.h"
-#include "pilha.h"
 
 Grafo criaGrafo(int n){
+	Grafo g;
+	g.lista_adj = calloc(n, sizeof(No*));
+	g.u = n;
+	return g;
+}
 
-} 
 void insereAresta(Grafo *g, int v, int u){
 
+
 }
+
+
 void buscaLargura(Grafo g, int s){
-
+	
 }
-void buscaProfundidade(Grafo g, int s){
 
-}
+
+ // g.Vet_adj = (no) malloc(n * sizeof(no));
--- grafo.h
+++ grafo.h
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct heap {
-	int elementos[MAX];//numero de elementos
-	int last;//Representação do Ultimo elemento
-} Heap;
 
-Grafo criaGrafo(int n); // Menor Valor
+
+typedef struct no {
+  int v; /* Vértice */
+  struct no* prox;
+} No;
+
+typedef struct grafo {
+  int u;      /* Número de nós */
+  No** lista_adj;
+} Grafo;
+
+
+Grafo criaGrafo(int n);
 void insereAresta(Grafo *g, int v, int u);
 void buscaLargura(Grafo g, int s);
 void buscaProfundidade(Grafo g, int s);
